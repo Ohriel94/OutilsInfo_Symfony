c4a31ccb141b3b449b9030ca5026e63aa26406f7
Ohriel94 <brendb@outlook.com>
2023-09-18 09:48:37 -0400

    Reorganisation
    - GestionOrdinateursController et GestionCellulairesController deplacer
    dans GestionController
    
    - gestion_ordinateur.html.twig renommer ordinateur.html.twig en et
    deplacer dans template/main/gestion
    
    - gestion_cellulaires.html.twig renommer cellulaires.html.twig en et
    deplacer dans template/main/gestion

---
+++ src/Controller/GestionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CellulaireRepository;
+use App\Repository\OrdinateurRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GestionOrdinateursController extends AbstractController
+{
+    #[Route('/gestion/ordinateurs', name: 'gestion_ordinateurs')]
+    public function Ordinateur(OrdinateurRepository $ordinateurRepository): Response
+    {
+        $ordinateurs = $ordinateurRepository->findAll();
+        return $this->render('main/gestion/ordinateur.html.twig', [
+            'controller_name' => 'GestionController',
+            'ordinateurs' => $ordinateurs
+        ]);
+    }
+    #[Route('/gestion/cellulaires', name: 'gestion_cellulaires')]
+    public function Cellulaire(CellulaireRepository $cellulaireRepository): Response
+    {
+        $cellulaires = $cellulaireRepository->findAll();
+        return $this->render('main/gestion/cellulaire.html.twig', [
+            'controller_name' => 'GestionController',
+            'cellulaires' => $cellulaires
+        ]);
+    }
+}
