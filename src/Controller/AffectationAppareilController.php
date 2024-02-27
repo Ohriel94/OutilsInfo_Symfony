1c4fe88b48339f753a959d9c139df0f9a06d2dfa
Brendan Dibert-Bekoy <brendb@outlook.com>
2024-02-27 12:23:01 -0500

    update

--- src/Controller/AffectationAppareilController.php
+++ src/Controller/AffectationAppareilController.php
@@ -2,60 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Cellulaire;
+use App\Entity\Ordinateur;
+use App\Entity\Peripherique;
+use App\Entity\Utilisateur;
 use App\Repository\OrdinateurRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Notifier\NotifierInterface;
 
 class AffectationAppareilController extends AbstractController
 {
-    #[Route('/affectation/appareil', name: 'app_affectation_appareil')]
-    public function index(OrdinateurRepository $ordinateurRepository, ): Response
-    {
-    //  const getUsers = () => {
-	//   console.log('getUsers');
-	//   const f = async () => {
-	//    try {
-	//     const getUsersRequest = await Axios({
-	//      method: 'get',
-	//      url: 'http://localhost:3001/usagers',
-	//     });
-	//     getUsersRequest.data.map((usager) => {
-	//      usager.label = usager.prenom + ' ' + usager.nom;
-	//     });
-	//     getUsersRequest.data.map((usager) => {
-	//      usager.appareilsAffectes.map((appareil) => {
-	//       appareil.id = `item-${Math.floor(Math.random() * 90000001)}`;
-	//      });
-	//     });
-	//     setUsagers(getUsersRequest.data);
-	//    } catch (e) {
-	//     console.log('Failed to connect ' + e);
-	//    }
-	//   };
-	//   f();
-	//  };
+	private $ordinateurRepository;
+    private $entityManager;
+    private $notifier;
 
-	//  const getOrdinateurs = () => {
-	//   const g = async () => {
-	//    try {
-	//     const getOrdinateursRequest = await Axios({
-	//      method: 'get',
-	//      url: 'http://localhost:3001/ordinateurs',
-	//     });
-	//     getOrdinateursRequest.data.map((ordinateur) => {
-	//      ordinateur.id = `item-${Math.floor(Math.random() * 90000001)}`;
-	//     });
-	//     setOrdinateurs(getOrdinateursRequest.data);
-	//    } catch (e) {
-	//     console.log('Failed to connect ' + e);
-	//    }
-	//   };
-	//   g();
-	//  };
+	public function __construct(EntityManagerInterface $entityManager, NotifierInterface $notifier) {
+        $this->entityManager = $entityManager;
+        $this->notifier = $notifier;
+    }
+
+    #[Route('/affectation/appareil', name: 'app_affectation_appareil')]
+    public function index(): Response
+    {
+		$ordinateurs = $this->entityManager->getRepository(Ordinateur::class)->findAll();
+		$cellulaires = $this->entityManager->getRepository(Cellulaire::class)->findAll();
+		$peripheriques = $this->entityManager->getRepository(Peripherique::class)->findAll();
+
+		$items = [];
+		$items["ordinateurs"] = $ordinateurs;
+		$items["cellulaires"] = $cellulaires;
+		$items["peripheriques"] = $peripheriques;
 
         return $this->render('affectation_appareil/index.html.twig', [
             'controller_name' => 'AffectationAppareilController',
+			'items' => $items,
         ]);
     }
 }
